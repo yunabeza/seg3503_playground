c79567427302617eb20a793a1b72950549c3150c
yunabeza <lolita.inabeza@gmail.com>
2023-06-29 12:52:53 -0400

    added comments, photos

--- lab4/fizzbuzz_java/src/fizzbuzz.java
+++ lab4/fizzbuzz_java/src/fizzbuzz.java
@@ -1,13 +1,16 @@
 public class fizzbuzz{
     final int nbr = 100;
     String sequence = "";
+
+    // generates the fizzbuzz sequence from 1 to 100
     public String generator() {
         for (int i = 1; i <= nbr; i++) {
-            sequence += FizzBuzzOrNum(i) + ", ";
+            sequence += FizzBuzzOrNum(i) + " ";
         }
         return sequence;
     }
 
+    //generates the fizzbuzz sequence from n to m
     public String generator(int n, int m) {
         for (int i = n; i <= m; i++) {
             sequence += FizzBuzzOrNum(i) + " ";
@@ -15,6 +18,7 @@ public class fizzbuzz{
         return sequence;
     }
 
+    //detects is the number is fizz, buzz, fizzbuzz, or none 
     public String FizzBuzzOrNum(int n){
          if (isMultipleOf3(n) && isMultipleOf5(n)) {
                 return "FizzBuzz";
